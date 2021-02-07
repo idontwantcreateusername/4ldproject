04571d997f79b88e7ea55f6445b1231513aa5693
LioShady <denispelipeychenko@yandex.com>
2021-02-07 12:58:15 +0300

    some updates

--- blocks.py
+++ blocks.py
@@ -10,6 +10,15 @@ PLATFORM_COLOR = "#FF6262"
 ICON_DIR = os.path.dirname(__file__)  # Полный путь к каталогу с файлами
 
 
+class Block(sprite.Sprite):
+    def __init__(self, x, y, img):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+        self.image.fill(Color(PLATFORM_COLOR))
+        self.image = image.load(img)
+        self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+
+
 class Platform(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
